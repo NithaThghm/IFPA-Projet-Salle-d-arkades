d4f6250cc482b573a543cc26a49c2e7e8e0a94e0
NithaThghm <t.nithachan@gmail.com>
2022-06-08 09:57:51 +0200

    First commit. + Navbar modified

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.addEventListener("click", e =>{
+    window.location.assign("./Page D'acceuil.html"); 
+})
+document.body.addEventListener("keypress", e =>{
+    window.location.assign("./Page D'acceuil.html"); 
+})
